62b0404c0c5f9cd434db826b49d28123a45836bf
jaogrs <jgabrielrodrigues2@gmail.com>
2022-04-28 14:47:34 -0300

    header

---
+++ app-jgrs/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <section>
+            <h2>joaogrs</h2>
+            <div/>
+        </section>
+    )
+}
+
+export default Header
