cb8dc5750cd40354799448820b8737dcc8f0aacf
jaogrs <jgabrielrodrigues2@gmail.com>
2022-05-05 22:20:02 -0300

    estilização inicial

--- app-jgrs/src/Components/Home.js
+++ app-jgrs/src/Components/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import '../styles/home.css'
 
 const Home = () => {
   return (
-    <section>
-      <p>Hi there, i'm 👋</p>
-      <h1>joaogrs</h1>
-      <p>Fullstack Developer 🧑🏾‍💻</p>
-      <p>
+    <section className='homeContainer'>
+      <p>HI THERE, I'M 👋</p>
+      <h1>joaogrs.</h1>
+      <p className='profession'>FULL STACK DEVELOPER 🧑🏾‍💻</p>
+      <p className='description'>
           I'm a Brazilian software developer passionate about what I do, and I believe that technology can change the country.
       </p>
       <button>About Me</button>
