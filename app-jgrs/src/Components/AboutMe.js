f71b12c66f2b504dbfe769c942114655fc5daefb
jaogrs <jgabrielrodrigues2@gmail.com>
2022-05-05 23:09:53 -0300

    estrutura about me

---
+++ app-jgrs/src/Components/AboutMe.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const AboutMe = () => {
+  <section className='aboutMe'>
+    <div className='wrapStart'>
+      <h1>
+        LOREM IPSUM
+      </h1>
+      <p>
+      Sed et tortor mollis, lacinia turpis in, mollis ipsum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque a scelerisque neque. Vestibulum at mi aliquam, vulputate massa a, condimentum arcu. Donec ac tempus dolor, egestas ornare eros. Nunc facilisis purus dui, ut ultrices risus feugiat maximus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+      </p>
+    </div>
+    <div className='wrapMedium'>
+      <ul>
+      <li><strong>Não é</strong>Sed et tortor mollis, lacinia turpis in, mollis ipsum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque a scelerisque neque. Vestibulum at mi aliquam, vulputate massa a, condimentum arcu. Donec ac tempus dolor, egestas ornare eros.</li>
+      <li><strong>Não é</strong>Sed et tortor mollis, lacinia turpis in, mollis ipsum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque a scelerisque neque. Vestibulum at mi aliquam, vulputate massa a, condimentum arcu. Donec ac tempus dolor, egestas ornare eros.</li>
+      <li><strong>Não é</strong>Sed et tortor mollis, lacinia turpis in, mollis ipsum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque a scelerisque neque. Vestibulum at mi aliquam, vulputate massa a, condimentum arcu. Donec ac tempus dolor, egestas ornare eros.</li>
+      <li><strong>Não é</strong>Sed et tortor mollis, lacinia turpis in, mollis ipsum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque a scelerisque neque. Vestibulum at mi aliquam, vulputate massa a, condimentum arcu. Donec ac tempus dolor, egestas ornare eros.</li>
+      <p>Sed et tortor mollis, lacinia turpis in, mollis ipsum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.</p>
+      </ul>
+    </div>
+  </section>
+
+}
+
+export default AboutMe;
